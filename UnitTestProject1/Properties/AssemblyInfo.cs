9f58fd480a0871f5292dcc8d4c0cce719be58720
PROG MISTERIO <habib2004habib@yandex.ru>
2022-04-14 02:42:22 +0300

    Добавьте файлы проекта.

---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8b1f373b-65a6-4bcc-a1da-7d68178ea840")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using calcul;
+
+
+namespace UnitTestProject1
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void Sum_10_5_15()
+		{
+			double n1 = 10d;
+			double n2 = 5d;
+			double n3 = 15d;
+
+			Assert.AreEqual(n3, Form1.sum(n1, n2));
+		}
+
+		[TestMethod]
+		public void Razn_10_5_5()
+		{
+			double n1 = 10d;
+			double n2 = 5d;
+			double n3 = 5d;
+
+			Assert.AreEqual(n3, Form1.razn(n1, n2));
+		}
+
+		[TestMethod]
+		public void Del_10_5_2()
+		{
+			double n1 = 10d;
+			double n2 = 5d;
+			double n3 = 2d;
+
+			Assert.AreEqual(n3, Form1.del(n1, n2));
+		}
+
+		[TestMethod]
+		public void Umnog_10_5_50()
+		{
+			double n1 = 10d;
+			double n2 = 5d;
+			double n3 = 50d;
+
+			Assert.AreEqual(n3, Form1.umnog(n1, n2));
+		}
+
+		[TestMethod]
+		public void Step_2_5_32()
+		{
+			double n1 = 2d;
+			double n2 = 5d;
+			double n3 = 32d;
+
+			Assert.AreEqual(n3, Form1.step(n1, n2));
+		}
+
+		[TestMethod]
+		public void Koren_16_2_4()
+		{
+			double n1 = 16d;
+			double n2 = 2d;
+			double n3 = 256d;
+
+			Assert.AreEqual(n3, Form1.step(n1, n2));
+		}
+	}
+}
